e12ab3724fa113bbfeaf4672ee8dd37457241482
Juan Carlos Migliavacca <juancarlos@xeonworks.com>
2023-04-09 10:04:13 -0300

    Add corporate theme

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,38 +1,31 @@
-import React, { useContext } from "react";
-import { MainContext } from "../../store/context/MainContext.jsx";
-import fetchHeroImage from "../../store/api/fetchHeroImage.jsx";
-import { useQuery } from "@tanstack/react-query";
-import BlockUi from "@availity/block-ui";
+import React from "react";
+import {Header} from "../../components/Header.jsx";
+import {Hero} from "../../components/Hero.jsx";
+import {PrimaryFeatures} from "../../components/PrimaryFeatures.jsx";
+import {SecondaryFeatures} from "../../components/SecondaryFeatures.jsx";
+import {CallToAction} from "../../components/CallToAction.jsx";
+import {Testimonials} from "../../components/Testimonials.jsx";
+import {Pricing} from "../../components/Pricing.jsx";
+import {Faqs} from "../../components/Faqs.jsx";
+import {Footer} from "../../components/Footer.jsx";
 
 
 const Home = (props) => {
-  const context = useContext(MainContext);
-  const response = useQuery(["heroImageRepo", 1], fetchHeroImage);
-
-
   return (
     <>
-      <BlockUi blocking={response.isLoading}>
-        <div className="rounded-lg border bg-white dark:border-neutral-600 dark:bg-neutral-800">
-          <div className="p-4">
-            <div className="grid grid-cols-2 items-center gap-2">
-              <div className="mb-8">
-                <div className="h-screen w-full flex justify-center items-center text-3xl">
-                  Home : {context.helloMessage}
-                </div>
-              </div>
-              <div className="mb-8">
-                <img
-                  src={response.data?.message}
-                  className="h-auto max-w-full rounded-full"
-                  alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </BlockUi>
+        <Header />
+        <main>
+          <Hero />
+          <PrimaryFeatures />
+          <SecondaryFeatures />
+          <CallToAction />
+          <Testimonials />
+          <Pricing />
+          <Faqs />
+        </main>
+        <Footer />
+      </>
 
-    </>
   );
 };
 

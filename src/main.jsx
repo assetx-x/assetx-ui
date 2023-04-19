b376033f34f8451160d78129b81f0f8915499a44
Juan Carlos Migliavacca <juancarlos@xeonworks.com>
2023-04-19 08:58:46 -0300

    Clean code

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import "@availity/block-ui/dist/index.css";

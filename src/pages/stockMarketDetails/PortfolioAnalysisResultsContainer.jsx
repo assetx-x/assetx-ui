f2bcd93f4c3fa91eca60199befdecbea438a44c9
Juan Carlos Migliavacca <juancarlos@xeonworks.com>
2023-07-07 00:49:17 -0300

    Add loader in portfolio transition

--- src/pages/stockMarketDetails/PortfolioAnalysisResultsContainer.jsx
+++ src/pages/stockMarketDetails/PortfolioAnalysisResultsContainer.jsx
@@ -4,6 +4,10 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import fetchHoldingsById from "../../store/models/validations/fetchHoldingsById.jsx";
 import PortfolioAnalysisResults from "./PortfolioAnalysisResults.jsx";
+import { Header } from "../../components/Header.jsx";
+import BlockUi from "@availity/block-ui";
+import { Loader } from "react-loaders";
+import { Container } from "../../components/Container.jsx";
 
 const PortfolioAnalysisResultsContainer = (props) => {
   const context = useMain();
@@ -15,7 +19,14 @@ const PortfolioAnalysisResultsContainer = (props) => {
     refetch
   } = useQuery(["portfolio", { id: params.id }], fetchHoldingsById);
 
-  if (!data) return <div>Loading...</div>;
+  if (!data) return  <>
+    <Header />
+    <BlockUi blocking={isLoading}   loader={<Loader active type="ball-scale" color="#0248C7" />}>
+      <Container />
+      <div className="flex items-center justify-center h-screen">
+      </div>
+    </BlockUi>
+  </>;
 console.log(data);
   return (
     <PortfolioAnalysisResults portfolio={data} />

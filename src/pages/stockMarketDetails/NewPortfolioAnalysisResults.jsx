65bc814760b1cbedba3e199de3655cf5c6edda22
Juan Carlos Migliavacca <juancarlos@xeonworks.com>
2023-08-31 07:21:38 -0300

    Fix footer style

--- src/pages/stockMarketDetails/NewPortfolioAnalysisResults.jsx
+++ src/pages/stockMarketDetails/NewPortfolioAnalysisResults.jsx
@@ -440,7 +440,7 @@ const NewPortfolioAnalysisResults = ({ portfolio }) => {
             </div>
 
 
-            <footer>
+            <footer className='flex'>
               {/*card*/}
               <div className="m-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">

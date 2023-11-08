4115de1b32fc36b106e20199889a8c88a83f7245
Juan Carlos Migliavacca <juancarlos@xeonworks.com>
2023-11-08 16:03:59 -0300

    Fix subtitles on sidebar

--- src/pages/stockMarketDetails/components/KeyValues.jsx
+++ src/pages/stockMarketDetails/components/KeyValues.jsx
@@ -10,22 +10,21 @@ const KeyValues = ({ data, selector }) => {
         className="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">{sentiment}</span>);
     }
   };
-
+  
   return (
     <section className="mb-20 mt-6">
 
       <div className="mt-6">
         <div className="relative overflow-x ">
           <div className="relative overflow-x-auto shadow-md sm:rounded-lg p-3 h-[780px]">
-            <h3 className="text-xl font-semibold">
-              Summary Values
-            </h3>
-            <h4 className="text-sm text-gray-500 font-light">Quality Factors</h4>
-
 
             <div className="w-full text-sm text-left text-gray-500">
               {Object.entries(data?.[selector]?.key_summary).map(([stock, factors]) => (
                 <div className="item" key={stock}>
+                  <h3 className="text-l font-bold text-gray-700 mt-6 ml-3">
+                    {stock} Summary Values
+                  </h3>
+                  <h4 className="text-sm text-gray-500 font-light ml-3">Quality Factors</h4>
                   {Object.entries(factors).map(([category, items]) => (
                     <ul className="bg-white border-b p-4 hover:bg-gray-50" key={category}>
                       <h3 className="font-bold">{category}</h3>

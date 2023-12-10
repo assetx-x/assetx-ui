e2fa8338966d4ba7cb6eee34b66b86659d00ad13
Tonny Jacklin Herison <hartonnyjack@gmail.com>
2023-12-10 23:50:27 +0300

    Revert "New interfaces"
    This reverts commit 73f30628fbded369c3369e7d998164dc8d7c8572.

--- src/components/AsymmetricErrorBarsWithConstantOffsetChart.jsx
+++ src/components/AsymmetricErrorBarsWithConstantOffsetChart.jsx
@@ -1,44 +1,47 @@
 import React, { useEffect } from "react";
 import Plot from "react-plotly.js";
 
-export function AsymmetricErrorBarsWithConstantOffsetChart({
-  data,
-  layoutParameters = {},
-}) {
+
+export function AsymmetricErrorBarsWithConstantOffsetChart({data, layoutParameters={}}) {
   // console.log('data', data)
-  useEffect(() => {}, [data]);
+  useEffect(() => {
+
+  }, [data]);
 
   let trace1 = {
-    x: data.pnl_.index,
-    y: data.pnl_.series,
+    x: data.benchmark.index,
+    y: data.benchmark.series,
     fillcolor: "rgba(226, 226, 226,0.7)",
-    line: { color: "#537FA0" },
+    line: {color: "#537FA0"},
     mode: "lines",
-    name: "Your portfolio",
-    type: "scatter",
+    name: "Ticker",
+    type: "scatter"
   };
 
   let trace2 = {
-    x: data.benchmark.index,
-    y: data.benchmark.series,
-    line: { color: "#537FA0", dash: "dot" },
+    x: data.pnl_.index,
+    y: data.pnl_.series,
+    line: {color: "rgb(226,226,226)",
+      dash: 'dot',},
     mode: "lines",
-    name: "AssetX re-weighted portfolio",
-    type: "scatter",
-  };
+    name: "SPY",
+    type: "scatter"
+  }
 
-  let finalData = [trace1, trace2];
+
+  let finalData = [trace1,
+    trace2];
   let layout = {
-    plot_bgcolor: "rgba(0,0,0,0)",
 
-    legend: { orientation: "h" },
+    plot_bgcolor:'rgba(0,0,0,0)',
+
+    legend: {"orientation": "h"},
     font: {
-      family: "NunitoSans-ExtraBold",
-      color: "#537FA0",
+      family: 'NunitoSans-ExtraBold',
+      color: '#537FA0'
     },
-    height: 800,
-    ...layoutParameters,
+    ...layoutParameters
   };
 
-  return <Plot data={finalData} style={{ width: "100%" }} layout={layout} />;
+  return <Plot data={finalData}  style={{width:"100%"}} layout={layout}/>;
 }
--- src/store/models/details/fetchFactorHack.jsx
+++
@@ -1,9 +0,0 @@
-import { getFactorHack } from "../../api/details.jsx";
-
-const fetchFactorHack = async ({ queryKey }) => {
-  const { ticker, factor } = queryKey;
-  const response = await getFactorHack(factor, ticker);
-  return response.data;
-};
-
-export default fetchFactorHack;
